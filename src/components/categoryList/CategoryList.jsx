e2c5c640ce2ec0274a1f4b297b5077c1ad764790
Yuresh Majhi <Yuresh Majhi>
2025-01-22 12:55:26 +0545

    Rough navbar & theme toggle

---
+++ src/components/categoryList/CategoryList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./categoryList.module.css";
+
+const CategoryList = () => {
+  return <div></div>;
+};
+
+export default CategoryList;
---
+++ src/components/pagination/Pagination.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./pagination.module.css";
+
+const Pagination = () => {
+  return <div></div>;
+};
+
+export default Pagination;
