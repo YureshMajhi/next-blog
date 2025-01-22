7e758754b58be71a887ec3879eb2b376e7cda7fd
Yuresh Majhi <Yuresh Majhi>
2025-01-22 21:02:49 +0545

    Toggle function complete

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -9,9 +9,24 @@ const ThemeToggle = () => {
   const { theme, toggle } = useContext(ThemeContext);
 
   return (
-    <div className={styles.container} onClick={toggle}>
+    <div
+      className={styles.container}
+      onClick={toggle}
+      style={
+        theme === "dark"
+          ? { backgroundColor: "white" }
+          : { backgroundColor: "#0f172a" }
+      }
+    >
       <Image src={"/moon.png"} alt="moon" width={14} height={14} />
-      <div className={styles.ball}></div>
+      <div
+        className={styles.ball}
+        style={
+          theme === "dark"
+            ? { backgroundColor: "#0f172a", left: 1 }
+            : { backgroundColor: "white", right: 1 }
+        }
+      ></div>
       <Image src={"/sun.png"} alt="sun" width={14} height={14} />
     </div>
   );
