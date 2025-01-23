bb44a46c3d1e3f3206140a4eddb558c8159eb483
Yuresh Majhi <Yuresh Majhi>
2025-01-23 12:12:18 +0545

    footer complete

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,53 @@
 import React from "react";
 import styles from "./footer.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image src={"/logo.png"} alt="_" width={100} height={80} />
+          <h1 className={styles.logoText}>Yureshblog</h1>
+        </div>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora aut
+          totam sequi error magni labore delectus voluptas voluptatum pariatur
+          quibusdam?
+        </p>
+        <div className={styles.icons}>
+          <Image src={"/facebook.png"} alt="_" width={18} height={18} />
+          <Image src={"/tiktok.png"} alt="_" width={18} height={18} />
+          <Image src={"/instagram.png"} alt="_" width={18} height={18} />
+          <Image src={"/youtube.png"} alt="_" width={18} height={18} />
+        </div>
+      </div>
+      <div className={styles.links}>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Links</span>
+          <Link href={"/"}>Homepage</Link>
+          <Link href={"/"}>About</Link>
+          <Link href={"/"}>Blog</Link>
+          <Link href={"/"}>Contact</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Tags</span>
+          <Link href={"/"}>Styles</Link>
+          <Link href={"/"}>Fashion</Link>
+          <Link href={"/"}>Coding</Link>
+          <Link href={"/"}>Travel</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Social</span>
+          <Link href={"/"}>Facebook</Link>
+          <Link href={"/"}>Tiktok</Link>
+          <Link href={"/"}>Instagram</Link>
+          <Link href={"/"}>Youtube</Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
